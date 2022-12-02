a07717b4cb4345eb73332ec4ff65313116a427f6
davidaryaseta <118499871+davidaryaseta@users.noreply.github.com>
2022-12-02 10:34:38 +0700

    Update 1_A_ 71220854.py

--- 1_A_ 71220854.py
+++ 1_A_ 71220854.py
@@ -1,25 +1,25 @@
-print=('======= Program Manipulasi String =======')
+print('======= Program Manipulasi String =======')
 
-menu=('Pilihan Menu :')
-menu=('1.Hitung Kata')
-menu=('2.Ubah Kata')
+print('Pilihan Menu :')
+print('1.Hitung Kata')
+print('2.Ubah Kata')
 menu = input('Pilihan anda : ')
 
 def hitung():
-    kalimat = input('masukan sebuah kalimat /kata :')
+    kalimat1 = input('masukan sebuah kalimat /kata :')
     hit = input('Masukan Kata yang ingin di hitung :')
-    a = kalimat.count(hit)
-    menu =('Terdapat sebanyak', a, 'kata', hit,'didalam kalimat')
+    a = kalimat1.count(hit)
+    print('Terdapat sebanyak', a, 'kata', hit,'didalam kalimat')
     return a
 
 def ubah():
     kalimat = input('Masukan sebuah kalimat kata : ')
-    mana = input ('Masukan kata yang ingin di ubah : ')
+    mana = input('Masukan kata yang ingin di ubah : ')
     pengganti = input('Masukan kata pengganti :')
 
     ganti = kalimat.replace(mana,pengganti)
 
-    print(f'String Berhasil Diubah menjadi {ganti}')
+    print('String Berhasil Diubah menjadi',ganti)
     return ganti 
 
 if menu == '1' :
